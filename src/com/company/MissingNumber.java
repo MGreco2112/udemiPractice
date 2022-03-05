da4700f0ccf85080c00dccb80dd8db074ae20407
Matthew Greco <mgreco2112@tutanota.com>
2022-03-04 20:20:04 -0500

    added instructors better solution

--- src/com/company/MissingNumber.java
+++ src/com/company/MissingNumber.java
@@ -5,16 +5,30 @@ import java.util.Arrays;
 public class MissingNumber {
 
     public static void main(String[] args) {
-        int[] array = new int[]{2, 3, 5, 1};
+        int[] array = new int[]{};
 
         System.out.println(solution(array));
     }
 
     public static int solution(int[] a) {
-        int total = (a.length + 1) * (a.length - 1);
-        int length = a.length;
+        //my attempt: works but not as well as his
 
+//        int total = (a.length + 1) * (a.length - 1);
+//        int length = a.length;
+//
+//
+//        return length % total;
 
-        return length % total;
+        long actualSum = 0;
+
+        for (int num : a) {
+            actualSum += num;
+        }
+
+        long maxNumber = a.length + 1;
+
+        long expectedSum = (maxNumber * (maxNumber + 1) / 2);
+
+        return (int) (expectedSum - actualSum);
     }
 }
