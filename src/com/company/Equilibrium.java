080ebd869e30a6cba1479580a889e9e6a8f30ea1
Matthew Greco <mgreco2112@tutanota.com>
2022-03-07 11:02:47 -0500

    better solution implemented with hints from video

--- src/com/company/Equilibrium.java
+++ src/com/company/Equilibrium.java
@@ -11,36 +11,71 @@ public class Equilibrium {
     }
 
     public static int solution(int[] a) {
-        int[] absValues = new int[a.length - 1];
+        int[][] results = new int[a.length - 1][2];
         int pointer = 1;
+        int currentDifference = Integer.MAX_VALUE;
+
+        System.out.println(Arrays.toString(a));
 
         while (pointer < a.length) {
-            int[] sums = new int[2];
 
-            for (int i = 0; i < a.length; i++) {
-                if (i < pointer) {
-                    sums[0] += a[i];
-                } else {
-                    sums[1] += a[i];
+            if (pointer == 1) {
+                for (int i = 0; i < a.length; i++) {
+                    if (i < pointer) {
+                        results[pointer - 1][0] += a[i];
+                    } else {
+                        results[pointer - 1][1] += a[i];
+                    }
                 }
+            } else {
+                results[pointer - 1][0] = results[pointer - 2][0] + a[pointer - 1];
+                results[pointer - 1][1] = results[pointer - 2][1] - a[pointer - 1];
             }
 
-            int value = sums[0] - sums[1];
-
-            absValues[pointer - 1] = Math.abs(value);
+            if (Math.abs(results[pointer - 1][0] - results[pointer - 1][1]) < currentDifference) {
+                currentDifference = Math.abs(results[pointer - 1][0] - results[pointer - 1][1]);
+            }
 
             pointer++;
+
         }
 
+        System.out.println(Arrays.deepToString(results));
 
-        int rtnVal = Integer.MAX_VALUE;
-
-        for (int absVal : absValues) {
-            if (absVal < rtnVal) {
-                rtnVal = absVal;
-            }
-        }
-
-        return rtnVal;
+        return currentDifference;
     }
+
+//    public static int solution(int[] a) {
+//        int[] absValues = new int[a.length - 1];
+//        int pointer = 1;
+//
+//        while (pointer < a.length) {
+//            int[] sums = new int[2];
+//
+//            for (int i = 0; i < a.length; i++) {
+//                if (i < pointer) {
+//                    sums[0] += a[i];
+//                } else {
+//                    sums[1] += a[i];
+//                }
+//            }
+//
+//            int value = sums[0] - sums[1];
+//
+//            absValues[pointer - 1] = Math.abs(value);
+//
+//            pointer++;
+//        }
+//
+//
+//        int rtnVal = Integer.MAX_VALUE;
+//
+//        for (int absVal : absValues) {
+//            if (absVal < rtnVal) {
+//                rtnVal = absVal;
+//            }
+//        }
+//
+//        return rtnVal;
+//    }
 }
